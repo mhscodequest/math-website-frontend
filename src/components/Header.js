d0e4e2b7f8666da678acc0e62eadd3c18e92face
jerrymiahdg <kingbebop15@gmail.com>
2023-11-23 09:23:20 -0800

    added tool tip

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,32 @@
 import "./Header.css";
 import Logo from "./Logo";
+import { useState } from "react";
+import ToolTip from "./ToolTip";
 
 const Header = () => {
+  const [isHover, setIsHover] = useState(false);
+
+  const mouseEnterHandler = () => {
+    setIsHover(true);
+  };
+  const mouseLeaveHandler = () => {
+    setIsHover(false);
+  };
+
   return (
     <div className="header">
       <div className="header_logo-container">
         <Logo />
         <h1 className="header_logo">MATHBOT</h1>
       </div>
-      <button className="header_btn">?</button>
+      <button
+        className="header_btn"
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler}
+      >
+        ?
+      </button>
+      <ToolTip className={`tool-tip ${isHover ? "show" : ""}`} />
     </div>
   );
 };
